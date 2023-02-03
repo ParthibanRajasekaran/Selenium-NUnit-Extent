c4654169d44cc77dd3006b6a69ccef594f9862c5
Parthiban Rajasekaran <parthibanrajasekaran@GBMB10258.local>
2023-02-03 13:27:21 +0000

    chore: add extend logs to the test cases

--- Selenium-Nunit-Extent/tests/CheckBoxTests.cs
+++ Selenium-Nunit-Extent/tests/CheckBoxTests.cs
@@ -1,4 +1,5 @@
-﻿using CSharpSelFramework.pageObjects;
+﻿using AventStack.ExtentReports;
+using CSharpSelFramework.pageObjects;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using selenium_nunit_extent.pageObjects;
@@ -8,6 +9,7 @@ using Selenium_Nunit_Extent.utils;
 namespace selenium_nunit_extent.tests
 {
     [TestFixture]
+    [Parallelizable(ParallelScope.Children)]
     public class CheckBoxTests : BaseTest
     {
         private HomePage homePage = new HomePage();
@@ -17,26 +19,29 @@ namespace selenium_nunit_extent.tests
         public void TestCheckCheckBoxes()
         {
             homePage.CheckBoxesPageLink.Click();
-            extentTest.Info("Checkboxes page is loaded successfully");
-
+            extentTest.Log(Status.Info, "Checkboxes page is loaded successfully");
             checkBoxPage.CheckBox1.Click();
+            extentTest.Log(Status.Info, "Checkbox 1 is checked on successfully");
 
             string labelText = checkBoxPage.CheckBox1.FindElement(By.XPath("..")).Text;
+            extentTest.Log(Status.Info, "Label text on the first checkbox is: " + labelText);
             Assert.IsTrue(checkBoxPage.CheckBox1.Selected, "The first checkbox is not checked.");
-            extentTest.Info("The first checkbox is checked successfully");
+            extentTest.Log(Status.Info, "The first checkbox is checked successfully");
         }
 
         [Test]
         public void TestUncheckCheckBoxes()
         {
             homePage.CheckBoxesPageLink.Click();
-            extentTest.Info("Checkboxes page is loaded successfully");
+            extentTest.Log(Status.Info, "Checkboxes page is loaded successfully");
 
             checkBoxPage.CheckBox2.Click();
+            extentTest.Log(Status.Info, "Checkbox 2 is unchecked successfully");
 
             string labelText = checkBoxPage.CheckBox2.FindElement(By.XPath("..")).Text;
+            extentTest.Log(Status.Info, "Label text on the second checkbox is: " + labelText);
             Assert.IsFalse(checkBoxPage.CheckBox2.Selected, "The second checkbox is checked.");
-            extentTest.Info("The second checkbox is unchecked successfully");
+            extentTest.Log(Status.Info, "The second checkbox is unchecked successfully");
         }
     }
 }
